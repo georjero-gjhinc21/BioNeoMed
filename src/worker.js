0ebac0a06b703b705fb22051e5ca3921a132f128
George Jerold V <georjero@gmail.com>
2026-04-07 19:46:44 -0500

    Worker: add CDN-Cache-Control, Surrogate-Control no-store headers

--- src/worker.js
+++ src/worker.js
@@ -8,7 +8,11 @@ export default {
       headers: {
         'Content-Type': 'text/html; charset=utf-8',
         'Cache-Control': 'no-store, no-cache, must-revalidate',
-        'Pragma': 'no-cache'
+        'Pragma': 'no-cache',
+        'Cloudflare-CDN-Cache-Control': 'no-store',
+        'CDN-Cache-Control': 'no-store',
+        'Surrogate-Control': 'no-store',
+        'Vary': 'Accept-Encoding'
       }
     });
   }
